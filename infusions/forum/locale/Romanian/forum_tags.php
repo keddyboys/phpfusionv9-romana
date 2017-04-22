b764f4f1cabd2235c25ab83ba79ae890e54bf9fd
keddyboys <keddyboys@gmail.com>
2017-04-22 22:23:15 +0300

    Update translation to revision 8432

---
+++ infusions/forum/locale/Romanian/forum_tags.php
@@ -0,0 +1,23 @@
+<?php
+
+$locale['forum_tag_0100'] = "Taguri forum";
+$locale['forum_tag_0101'] = "Tagurile sunt utilizate pt organizarea discu&#355;iilor tale";
+$locale['forum_tag_0102'] = "Taguri curente forum";
+$locale['forum_tag_0103'] = "Creaz&#259; tag";
+$locale['forum_tag_0104'] = "Editeaz&#259; tag";
+$locale['forum_tag_0105'] = "Tag creat";
+$locale['forum_tag_0106'] = "Tag actualizat";
+$locale['forum_tag_0107'] = "Tag &#351;ters";
+
+$locale['forum_tag_0110'] = "Informat&#355;i sait";
+$locale['forum_tag_0111'] = "Discu&#355;ii cu privire la managementul acestui sait";
+
+$locale['forum_tag_0200'] = "Titlu tag";
+$locale['forum_tag_0201'] = "Descriere tag";
+$locale['forum_tag_0202'] = "Culoare tag";
+$locale['forum_tag_0203'] = "Limb&#259; tag";
+$locale['forum_tag_0204'] = "Status tag";
+$locale['forum_tag_0205'] = "Acest tag este activ";
+$locale['forum_tag_0206'] = "Acest tag este inactiv";
+$locale['forum_tag_0207'] = "Creaz&#259; tag";
+$locale['forum_tag_0208'] = "Actualizeaz&#259; tag";
---
+++ infusions/forum_mods_online_panel/locale/Romanian.php
@@ -0,0 +1,7 @@
+<?php
+
+$locale['fmp_0100'] = "Echip&#259; online acum";//Staff Online Now";
+$locale['fmp_0101'] = "Echip&#259; management";//Management Staff";
+$locale['fmp_0102'] = "Moderatori online acum";
+$locale['fmp_0103'] = "Te rog instaleaz&#259; forumul";
+?>
