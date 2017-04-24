22a20caff87d4733303ea502ebf5fae005687c7b
keddyboys <keddyboys@gmail.com>
2017-04-24 12:33:11 +0300

    Adaugat user_fields in romana

--- locale/Romanian/user_fields/user_location.php
+++ locale/Romanian/user_fields/user_location.php
@@ -1,5 +1,5 @@
 <?php
 $locale['uf_location'] = "Loca&#355;ie";
 $locale['uf_location_desc'] = "&#354;ara, ora&#351;";
-$locale['uf_location_error'] = "Eroare: Nu a fost setat&#259; loca&#355;ia.";
+$locale['uf_location_error'] = "C&#226;mpul loca&#355;ie nu poate l&#259;sat gol.";
 ?>
--- locale/Romanian/user_fields/user_yahoo.php
+++ locale/Romanian/user_fields/user_yahoo.php
@@ -1,5 +1,5 @@
 <?php
 $locale['uf_yahoo'] = "Yahoo!";
 $locale['uf_yahoo_desc'] = "Yahoo! Instant Messenger";
-$locale['uf_yahoo_error'] = "Eroare: C&#226;mpul 'Yahoo' nu poate fi l&#259;sat gol.";
+$locale['uf_yahoo_error'] = "C&#226;mpul 'Yahoo' nu poate fi l&#259;sat gol.";
 ?>
